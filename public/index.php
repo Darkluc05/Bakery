c53e59a057e9679b4f6015e03a21b331b761200c
Luc <34852@ma-web.nl>
2023-11-06 16:17:43 +0100

    start project bakkery/ database set up

---
+++ public/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/f21cc9e720.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="./js/main.js" defer></script>
+    <title></title>
+</head>
+<body>
+    <header class="header">
+        <h1 class="header__title">
+            Bakkerij van Luc
+        </h1>
+    </header>
+    <main>
+
+        <article class="product">
+            <h3 class="product__title"></h3>
+        </article>
+
+
+    </main>
+</body>
+</html>
---
+++ public/system/database.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "mariadb";
+$username = "root";
+$password = "4dy5qwtrsag#!sad";
+$database = "bakkerij";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "hello";
